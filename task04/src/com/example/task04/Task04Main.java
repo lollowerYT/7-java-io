07b404f473792328077c3d3eab3c4478d96682e3
lollowerYT <77741489+lollowerYT@users.noreply.github.com>
2025-10-15 20:49:40 +0500

    Update Task04Main.java

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,14 +1,29 @@
 package com.example.task04;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - запустить программу
-        // - проверить, что получилось 351.731900
-
-        System.out.println("0.0");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        double sum = 0.0;
+        
+        String line;
+        while ((line = reader.readLine()) != null) {
+            StringTokenizer tokenizer = new StringTokenizer(line);
+            
+            while (tokenizer.hasMoreTokens()) {
+                String token = tokenizer.nextToken();
+                try {
+                    double number = Double.parseDouble(token);
+                    sum += number;
+                } catch (NumberFormatException e) {
+                }
+            }
+        }
+        
+        System.out.printf("%.6f%n", sum);
     }
 }
