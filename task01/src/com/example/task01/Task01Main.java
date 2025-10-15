2b0c612a84f0703e2207e3980739d978679d5ac6
lollowerYT <77741489+lollowerYT@users.noreply.github.com>
2025-10-15 20:41:47 +0500

    Update Task01Main.java

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -5,17 +5,21 @@ import java.io.InputStream;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
-        System.out.println(checkSumOfStream(new ByteArrayInputStream(new byte[]{0x33, 0x45, 0x01})));
-        */
-
+        System.out.println(checkSumOfStream(
+            new ByteArrayInputStream(new byte[]{0x33, 0x45, 0x01})
+        ));
     }
 
     public static int checkSumOfStream(InputStream inputStream) throws IOException {
-        // your implementation here
-        return 0;
+        int checksum = 0;
+        
+        int currentByte;
+        while ((currentByte = inputStream.read()) != -1) {
+            // Применяем формулу: C[n+1] = rotateLeft(C[n]) xor b[n+1]
+            checksum = Integer.rotateLeft(checksum, 1);
+            checksum ^= currentByte;
+        }
+        
+        return checksum;
     }
 }
