e38e2b544dd3353c3e24d2b7a600de35268d55bf
lollowerYT <77741489+lollowerYT@users.noreply.github.com>
2025-10-15 20:45:22 +0500

    Update Task02Main.java

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,13 +1,39 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+        InputStream input = System.in;
+        OutputStream output = System.out;
+        
+        // Байт-коды для перевода строк
+        final byte CR = 13; // \r
+        final byte LF = 10; // \n
+        
+        byte currentByte;
+        byte nextByte = -1;
+        
+        while ((currentByte = input.read()) != -1) {
+            if (input.available() > 0) {
+                nextByte = input.read();
+            } else {
+                nextByte = -1;
+            }
+            
+            if (currentByte == CR && nextByte == LF) {
+                output.write(LF);
+            } else {
+                output.write(currentByte);
+                
+                if (nextByte != -1) {
+                    input.unread(nextByte);
+                }
+            }
+        }
+        
+        output.flush();
     }
 }
